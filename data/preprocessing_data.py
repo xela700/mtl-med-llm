fd724a9cc66ff9aefa59fa82e9e96a976d5d435e
Alex Nicholson <apn2966@rit.edu>
2025-07-09 23:55:37 -0400

    Converting preprocessing methods to a wrapper class for reuse

--- data/preprocessing_data.py
+++ data/preprocessing_data.py
@@ -5,87 +5,151 @@ and clinical note summarization.
 
 import re
 import logging
+from transformers import AutoTokenizer
+from transformers import DataCollatorWithPadding
+from datasets import load_dataset
+from torch.utils.data import Dataset
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 
 
 logger = logging.getLogger(__name__)
 
-def remove_blank_sections(note: str, sections_headers: list[str] = None) -> str:
+class TextPreprocessor:
     """
-    Removed redacted sections from MIMIC data that occur as a result of the
-    deindentification process.
+    Parent class for preprocessing text imported from dataset.
 
-    Parameters:
-    note (str): clinical note to be stripped of unneeded sections
-    sections_headers (list[str]): Sections that will be checked for removal. Base sections from MIMIC will be used if none provided
-
-    Return:
-    str: clinical note stripped of blank sections
+    Attributes:
+    checkpoint (str): The associated HuggingFace model. Must match model used in training.
+    text_col (str): Column that contains the text to be cleaned and tokenized
+    section_headers (list[str]): Optional section headers used to remove sections of clinical
+    notes. Default of 'None' results in removal of sections that will always be blank due to 
+    de-identification
     """
 
-    if sections_headers is None:
-        logger.info("Using default section headers to clean data")
-        sections_headers = [
-            "Name:", "Unit No:", "Admission Date:", 
-            "Discharge Date:", "Date of Birth", "Attending:",
-            "Social History:", "Followup Instructions:", "Facility:" 
-        ]
+    __slots__ = ("tokenizer", "text_col", "section_headers")
+
+    def __init__(self, checkpoint: str, text_col: str, sections_headers: list[str] = None):
+        self.tokenizer = self._tokenize_data(checkpoint)
+        self.text_col = text_col
+        self.section_headers = sections_headers
+        self.data_collator = DataCollatorWithPadding(tokenizer=self.tokenizer)
+
     
-    lines = note.split("\n")
-    cleaned = []
-    i = 0
+    def _tokenize_data(self, checkpoint: str) -> PreTrainedTokenizerBase:
+        """
+        Initializes the tokenizer to be used in preprocessing.
 
-    while i < len(lines):
-        line = lines[i].strip()
+        Parameters:
+        checkpoint (str): Checkpoint to be used to determine the model. Must be the 
+        same as the checkpoint used in training.
 
-        if line == "":
-            i +=1
-            continue
+        Returns:
+        PreTrainedTokenizerBase: tokenizer for the model
 
-        if any(header.lower() in line.lower() for header in sections_headers):
-            # Captures and removes sections followed by underscore blanks
-            if re.fullmatch(r".*:\s*_+\s*", line):
-                i += 1
-                continue
-            # Captures and removes sections with underscore blanks on the following line
-            elif i + 1 < len(lines) and re.fullmatch(r"^_+\s*", lines[i + 1].strip()):
-                i += 2
-                continue
-            # Catch all for a header that should be removed
-            else:
-                i += 1
-                continue
+        Raises:
+        ValueError/OSError if checkpoint provided does not exist or is inaccessible
+        """
+
+        try:
+            tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+        except (ValueError, OSError) as err:
+            logger.error(f"Unable to access model using checkpoint {checkpoint}")
+            raise ValueError(f"Unable to access model using checkpoint {checkpoint}") from err
         
-        else:
-            cleaned.append(lines[i])
-            i += 1
+        return tokenizer
+
+    def remove_blank_sections(self, note: str) -> str:
+        """
+        Removed redacted sections from MIMIC data that occur as a result of the
+        deindentification process.
+
+        Parameters:
+        note (str): clinical note to be stripped of unneeded sections
+
+        Return:
+        str: clinical note stripped of blank sections
+        """
+
+        if self.sections_headers is None:
+            logger.info("Using default section headers to clean data")
+            self.sections_headers = [
+                "Name:", "Unit No:", "Admission Date:", 
+                "Discharge Date:", "Date of Birth", "Attending:",
+                "Social History:", "Followup Instructions:", "Facility:" 
+            ]
+        
+        lines = note.split("\n")
+        cleaned = []
+        i = 0
+
+        while i < len(lines):
+            line = lines[i].strip()
+
+            if line == "":
+                i +=1
+                continue
+
+            if any(header.lower() in line.lower() for header in self.sections_headers):
+                # Captures and removes sections followed by underscore blanks
+                if re.fullmatch(r".*:\s*_+\s*", line):
+                    i += 1
+                    continue
+                # Captures and removes sections with underscore blanks on the following line
+                elif i + 1 < len(lines) and re.fullmatch(r"^_+\s*", lines[i + 1].strip()):
+                    i += 2
+                    continue
+                # Catch all for a header that should be removed
+                else:
+                    i += 1
+                    continue
+            
+            else:
+                cleaned.append(lines[i])
+                i += 1
+        
+        return "\n".join(cleaned)
     
-    return "\n".join(cleaned)
 
+    def normalize_deidentified_blanks(self, text: str) -> str:
+        """
+        Inserts standard placeholder text inplace of de-identified blanks where applicable.
 
-def normalize_deidentified_blanks(text: str) -> str:
-    """
-    Inserts standard placeholder text inplace of de-identified blanks where applicable
+        Parameters:
+        text (str): Input text w/ de-identified blanks from MIMIC notes
 
-    Parameters:
-    text (str): Input text w/ de-identified blanks from MIMIC notes
+        Returns:
+        str: Text w/ replacement placeholders for training
+        """
 
-    Returns:
-    str: Text w/ replacement placeholders for training
-    """
+        replacements = {
+            r"\b_{2,}\s*(years?\s*old|y\.?o\.?|y/o|yo)": "<AGE>",
+            r"\b_{2,}-years?-old\b": "<AGE>",
+            r"\b(mrs?|ms)\.?\s_{2,}": "<PATIENT>",
+            r"\b(doctor|dr.?)\s_{2,}": "<DOCTOR>",
 
-    replacements = {
-        r"\b_{2,}\s*(years?\s*old|y\.?o\.?|y/o|yo)": "<AGE>",
-        r"\b_{2,}-years?-old\b": "<AGE>",
-        r"\b(mrs?|ms)\.?\s_{2,}": "<PATIENT>",
-        r"\b(doctor|dr.?)\s_{2,}": "<DOCTOR>",
+        }
 
-    }
-
-    for pattern, replacement in replacements.items():
-        text = re.sub(pattern, replacement, text, flags=re.IGNORECASE)
+        for pattern, replacement in replacements.items():
+            text = re.sub(pattern, replacement, text, flags=re.IGNORECASE)
+        
+        text = re.sub(r"\b_{2,}\b", "<REDACTED>", text) # Clean up for remaining unknown de-identified fields
+        return text
     
-    text = re.sub(r"\b_{2,}\b", "<REDACTED>", text) # Clean up for remaining unknown de-identified fields
-    return text
+
+    def tokenize_function(self, batch: dict[list[str]]):
+        """
+        Tokenizer function to be mapped using dataset.
+
+        Parameters:
+        batch (dict[list[str]]): 
+
+        Returns:
+        Tokenized return based on tokenizer usage.
+        """
+
+        return self.tokenizer(batch[self.text_col], truncation=True)
+    
+
 
 
 def classification_extract_sections(text: str, sections: list[str] = None) -> str:
@@ -115,3 +179,11 @@ def classification_extract_sections(text: str, sections: list[str] = None) -> st
             parsed_text[section] = match.group(1).strip()
     
     return ' '.join(filter(None, parsed_text.values()))
+
+
+
+    
+
+
+
+    
