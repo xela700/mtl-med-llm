fbaf65040c53ed690025af33aa950bb9011db425
Alex Nicholson <apn2966@rit.edu>
2025-11-01 15:29:53 -0400

    Classification model w/o code label embeddings. MoE: n=8, k=4. LoRA: r=32, alpha=64

--- model/train_model.py
+++ model/train_model.py
@@ -91,10 +91,10 @@ def classification_model_training(data_dir: str, label_mapping_dir: str, active_
         )
 
         lora_config = LoraConfig( # PERF tuning
-            r=8, # Increased rank from 8
-            lora_alpha=16, # quadrupled from 16 to 64
+            r=32, # Increased rank from 8
+            lora_alpha=64, # quadrupled from 16 to 64
             target_modules=["query", "value"],
-            lora_dropout=0.1, # Lowered from 0.1
+            lora_dropout=0.05, # Lowered from 0.1
             bias="none",
             task_type=TaskType.SEQ_CLS
         )
@@ -127,7 +127,7 @@ def classification_model_training(data_dir: str, label_mapping_dir: str, active_
         model = CodelessWrapper(
             config=config, 
             base_encoder=base_encoder, 
-            label_embeds=label_embeds,
+            num_labels=num_labels,
             pos_weight=pos_weight,
             active_label_mask=mask
             )
@@ -518,8 +518,8 @@ class CodelessWrapper(PreTrainedModel):
         self.proj = MoEProjectionLayer(
             input_dim=hidden_dim,
             hidden_dim=proj_hidden,
-            num_experts=4,
-            top_k=2,
+            num_experts=8,
+            top_k=4,
             dropout=0.2
         )
 
